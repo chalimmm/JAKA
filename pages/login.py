26beb784e079b9b9a9adbb17d8cdbefc83f1ef9e
fdsyawalino <90434721+fdsyawalino@users.noreply.github.com>
2021-11-08 23:03:03 +0700

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -13,32 +13,21 @@ def auth(username, password):
 def app():
     st.markdown("<h1 style='text-align: center;'>JAKA</h1>", unsafe_allow_html=True)
     st.markdown("<p style='text-align: center;'>Sign in to your JAKA account.</p>", unsafe_allow_html=True)
-    #st.title('JAKA')
-    #st.write('Sign In to your JAKA account')
+    st.title('JAKA')
+    st.write('Sign In to your JAKA account')
 
-#     with st.form('login_form', clear_on_submit=True):
-#         u = st.text_input('Username', max_chars=30)
-#         p = st.text_input('Password', type='password', max_chars=30)
-#         is_agree = st.checkbox('Saya telah membaca dan menyetujui Kebijakan Privasi JAKA')
-# #         col1, col2, col3  = st.columns((3,1,3))
-# #         with col1:
-# #             pass
-# #         with col3:
-# #             pass
-# #         with col2 :
-#         is_login = st.form_submit_button('Login')
-    col1, col2, col3, col4, col5  = st.columns((5))
-    with col1:
-        pass
-    with col2:
-        pass
-    with col4:
-        pass
-    with col5:
-        pass
-    with col3 :
-        st.button("Login")
-    if is_agree and is_login:
-        return auth(u, p)
-    else:
-        return False
+    with st.form('login_form', clear_on_submit=True):
+        u = st.text_input('Username', max_chars=30)
+        p = st.text_input('Password', type='password', max_chars=30)
+        is_agree = st.checkbox('Saya telah membaca dan menyetujui Kebijakan Privasi JAKA')
+        col1, col2, col3  = st.columns((3,1,3))
+        with col1:
+            pass
+        with col3:
+            pass
+        with col2 :
+            is_login = st.form_submit_button('Login')
+#     if is_agree and is_login:
+#         return auth(u, p)
+#     else:
+#         return False
