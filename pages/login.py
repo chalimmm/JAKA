4fc0980f6bc45410909bf62f3821669191f53280
fdsyawalino <90434721+fdsyawalino@users.noreply.github.com>
2021-11-08 22:52:48 +0700

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -20,7 +20,7 @@ def app():
         u = st.text_input('Username', max_chars=30)
         p = st.text_input('Password', type='password', max_chars=30)
         is_agree = st.checkbox('Saya telah membaca dan menyetujui Kebijakan Privasi JAKA')
-        col1, col2, col3  = st.columns((3,2,3))
+        col1, col2, col3  = st.columns((3,3,2))
         with col1:
             pass
         with col3:
