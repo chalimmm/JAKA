d757c855959b44c010a8faea24a505aba0d782fa
fdsyawalino <90434721+fdsyawalino@users.noreply.github.com>
2021-11-08 22:57:42 +0700

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -16,18 +16,24 @@ def app():
     #st.title('JAKA')
     #st.write('Sign In to your JAKA account')
 
-    with st.form('login_form', clear_on_submit=True):
-        u = st.text_input('Username', max_chars=30)
-        p = st.text_input('Password', type='password', max_chars=30)
-        is_agree = st.checkbox('Saya telah membaca dan menyetujui Kebijakan Privasi JAKA')
-#         col1, col2, col3  = st.columns((3,1,3))
-#         with col1:
-#             pass
-#         with col3:
-#             pass
-#         with col2 :
-    is_login = st.form_submit_button('Login')
-
+#     with st.form('login_form', clear_on_submit=True):
+#         u = st.text_input('Username', max_chars=30)
+#         p = st.text_input('Password', type='password', max_chars=30)
+#         is_agree = st.checkbox('Saya telah membaca dan menyetujui Kebijakan Privasi JAKA')
+# #         col1, col2, col3  = st.columns((3,1,3))
+# #         with col1:
+# #             pass
+# #         with col3:
+# #             pass
+# #         with col2 :
+#         is_login = st.form_submit_button('Login')
+    col1, col2, col3  = st.columns((3,1,3))
+    with col1:
+        pass
+    with col3:
+        pass
+    with col2 :
+        st.button("Login")
     if is_agree and is_login:
         return auth(u, p)
     else:
