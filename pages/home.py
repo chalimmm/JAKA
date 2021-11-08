073872dcac569ea921966bc2bdcd5a6105b025f4
fdsyawalino <90434721+fdsyawalino@users.noreply.github.com>
2021-11-08 21:42:57 +0700

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -70,7 +70,7 @@ def app():
             body.success('Cieee udah ready nih')
         else:
             body.title("JAKA", 'get-started')
-            st.write("Jadwal Aman, Kuliah Aman.")
+            body.write("Jadwal Aman, Kuliah Aman.")
             about_text = """
                 Jaka is built to make Universitas Indonesia student's course scheduling easier, faster, seamless, and more intuitive than ever.
             """
