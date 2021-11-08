fb800b43fe2663b5ce116944d20bfefc7bb52704
fdsyawalino <90434721+fdsyawalino@users.noreply.github.com>
2021-11-08 22:30:36 +0700

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -80,6 +80,7 @@ def app():
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;800&display=swap" rel="stylesheet">
+        <style>
         p {
         font-family: 'Montserrat';font-size: 25px;
         }
