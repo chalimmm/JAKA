492fcbf68dfde85209ce7608505ea4c458cf74f8
fdsyawalino <90434721+fdsyawalino@users.noreply.github.com>
2021-11-08 22:22:08 +0700

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -70,7 +70,7 @@ def app():
         <style>
         h1 {
         font-family: 'Montserrat';font-size: 40px;
-        font-weight: 1000;
+        font-weight: 4000;
         }
         p {
         font-family: 'Montserrat';font-size: 25px;
