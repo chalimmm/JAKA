45cfd9286b269d55434dbbb29f8278327ca021b0
Chandra Halim Nuruddin <39824546+chalimmm@users.noreply.github.com>
2021-11-10 01:10:52 +0700

    Update index.py

--- index.py
+++ index.py
@@ -22,16 +22,24 @@ page_list = [
     # ,'View Plan'    
 ]
 st.markdown("""
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&display=swap" rel="stylesheet">
 <style>
 div.stButton > button {
     background-color: #f72585;
-    border: none;
+    border-radius: 50px;
+	display: inline-block;
+	border: none;
+	transition: all 0.4s ease 0s;
     color: white;
     padding: 15px 32px;
     text-align: center;
+    text-transform: uppercase;
     text-decoration: none;
     display: inline-block;
-    font-size: 16px;
+    font-family: 'Montserrat';
+    font-weight: 700;
     margin: 4px 2px;
     cursor: pointer;
     width: 100%;
@@ -39,6 +47,17 @@ div.stButton > button {
 div.stButton > button:hover {
     background-color: #f8f8f8;
     box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 10px 20px 0 rgba(0,0,0,0.19);
+}
+.st-ae,
+.st-af,
+.st-ag,
+.st-ah,
+.st-ai,
+.st-aj,
+.st-bb,
+.st-bc,
+.st-b7{
+    cursor: pointer;
 }
 </style>""", unsafe_allow_html=True)
 
@@ -58,19 +77,8 @@ st.markdown("""
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
 <style>
-p {
-font-family: 'Montserrat';font-size: 12px;
-}
-</style>""", unsafe_allow_html=True)
-
-st.markdown("""
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
-<style>
-a {
-font-family: 'Montserrat';font-size: 25px;
-}
+p { font-family: 'Montserrat';font-size: 12px; }
+a { font-family: 'Montserrat';font-size: 25px; }
 </style>""", unsafe_allow_html=True)
 
 h1, empty, h2 = st.columns((1, 4, 1))
