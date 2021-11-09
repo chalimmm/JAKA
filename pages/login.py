8af73efbb5c62c3ac0e2ed55b9012324208c19aa
Chandra Halim Nuruddin <39824546+chalimmm@users.noreply.github.com>
2021-11-10 02:40:35 +0700

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -7,30 +7,26 @@ def auth(username, password):
     if username == check_u and password == check_p:
         st.success('Login Successful')
     else:
-        st.warning('Login Unsuccessful')
+        st.error('Login Unsuccessful')
     return True
 
 def app():
     st.markdown("<h1 style='text-align: center;'>JAKA</h1>", unsafe_allow_html=True)
-    st.markdown("<center><a style='text-align: center;'>Sign in to your JAKA account.</a></center>", unsafe_allow_html=True)
+    st.markdown("<center><a style='text-align: center;'>Sign in to your SSO-UI account.</a></center>", unsafe_allow_html=True)
     st.write(" ")
     st.write(" ")
-    with st.form('login_form', clear_on_submit=True):
-        u = st.text_input('Username', max_chars=30)
-        p = st.text_input('Password', type='password', max_chars=30)
-        with st.expander("See explanation"):
-            st.write("""
-                The chart above shows some numbers I picked for you.
-                I rolled actual dice for these, so they're *guaranteed* to
-                be random.
-            """)
-        is_agree = st.checkbox('Saya telah membaca dan menyetujui Kebijakan Privasi JAKA')
-        col1, col2, col3  = st.columns((3,1,3))
-        with col1:
-            pass
-        with col3:
-            pass
-        with col2 :
+    left, center, right = st.columns([2, 3, 2])
+    with center:
+        with st.form('login_form', clear_on_submit=False):
+            u = st.text_input('Username', max_chars=30)
+            p = st.text_input('Password', type='password', max_chars=30)
+            with st.expander("Kebijakan Privasi JAKA"):
+                st.write("""
+                    The chart above shows some numbers I picked for you.
+                    I rolled actual dice for these, so they're *guaranteed* to
+                    be random.
+                """)
+            is_agree = st.checkbox('Saya telah membaca dan menyetujui Kebijakan Privasi JAKA')
             is_login = st.form_submit_button('Login')
     if is_agree and is_login:
         return auth(u, p)
