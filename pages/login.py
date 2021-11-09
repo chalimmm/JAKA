ba7afaab0721903613273564f371f4c6b90fbd7f
Chandra Halim Nuruddin <39824546+chalimmm@users.noreply.github.com>
2021-11-10 03:54:07 +0700

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -3,32 +3,35 @@ import streamlit as st
 check_u = 'admin'
 check_p = 'admin'
 
-def auth(username, password):
-    if username == check_u and password == check_p:
-        st.success('Login Successful')
-    else:
-        st.error('Login Unsuccessful')
-    return True
+def auth(u, p):
+    st.session_state['logged_in'] = u == check_u and p == check_p and st.session_state['isAgree']
 
 def app():
     st.markdown("<h1 style='text-align: center;'>JAKA</h1>", unsafe_allow_html=True)
     st.markdown("<center><a style='text-align: center;'>Sign in to your SSO-UI account.</a></center>", unsafe_allow_html=True)
     st.write(" ")
     st.write(" ")
-    left, center, right = st.columns([2, 3, 2])
+    
+    left, center, right = st.columns([1, 2, 1])
     with center:
-        with st.form('login_form', clear_on_submit=False):
-            u = st.text_input('Username', max_chars=30)
-            p = st.text_input('Password', type='password', max_chars=30)
-            with st.expander("Kebijakan Privasi JAKA"):
-                st.write("""
-                    The chart above shows some numbers I picked for you.
-                    I rolled actual dice for these, so they're *guaranteed* to
-                    be random.
-                """)
-            is_agree = st.checkbox('Saya telah membaca dan menyetujui Kebijakan Privasi JAKA')
-            is_login = st.form_submit_button('Login')
-    if is_agree and is_login:
-        return auth(u, p)
-    else:
-        return False
+        # with st.form('login_form'):
+        #     u = st.text_input('Username', max_chars=30)
+        #     p = st.text_input('Password', type='password', max_chars=30)
+        #     with st.expander("Kebijakan Privasi JAKA"):
+        #         st.write("""
+        #             The chart above shows some numbers I picked for you.
+        #             I rolled actual dice for these, so they're *guaranteed* to
+        #             be random.
+        #         """)
+        #     st.session_state['isAgree'] = st.checkbox('Saya telah membaca dan menyetujui Kebijakan Privasi JAKA')
+        #     st.form_submit_button('Login', on_click=auth(u, p))
+        u = st.text_input('Username', max_chars=30)
+        p = st.text_input('Password', type='password', max_chars=30)
+        with st.expander("Kebijakan Privasi JAKA"):
+            st.write("""
+                The chart above shows some numbers I picked for you.
+                I rolled actual dice for these, so they're *guaranteed* to
+                be random.
+            """)
+        st.session_state['isAgree'] = st.checkbox('Saya telah membaca dan menyetujui Kebijakan Privasi JAKA')
+        st.button('Login', on_click=auth(u, p))
