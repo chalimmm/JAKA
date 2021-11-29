08975c89b5bf6c2ac186d822500566351a17e1fc
Chandra Halim Nuruddin <39824546+chalimmm@users.noreply.github.com>
2021-11-29 23:54:06 +0700

    Delete SeleniumBase directory

--- SeleniumBase/scraping.py
+++
@@ -1,6 +0,0 @@
-from seleniumbase import BaseCase
-
-class DemoSiteTests(BaseCase):
-    def test_demo_site(self):
-        self.open("https://academic.ui.ac.id/main/Authentication/")
-        self.sleep(10)
