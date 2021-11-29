027e51ba63287c00b21610c9711f00fdbf29f656
Chandra Halim Nuruddin <39824546+chalimmm@users.noreply.github.com>
2021-11-30 06:23:30 +0700

    Update schedule.py

--- pages/schedule.py
+++ pages/schedule.py
@@ -3,30 +3,29 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-def FilterCourse():
+def app():
     db = firestore.client()
     
-    courses = db.collection('users').document(st.session_state['username']).get()
-    courses = courses.to_dict()
-    courses = courses['listCourse']
+    listCourse = db.collection('users').document(st.session_state['username']).get()
     
-    for course in courses:
+    courses = {}
+    st.session_state['selectedCourse'] = []
+    
+    for course in listCourse:
         getData = db.collection('courses').document(course).get()
         if getData.exists:
             getData = getData.to_dict()
-            if courses[course] != getData:
-                print("UPDATE : " + str(course))
-                db.collection('courses').document(course).update(courses[course])
-        else:
-            db.collection('courses').document(course).set(courses[course])
-    
+            courses[course].set(getData)
+            
     c1, c2 = st.columns((3, 1))
+    
     with c1:
-        course_list = []
+        listCourse = []
         for course in courses:
-            course_list.append(course['name']+' ('+str(course['sks'])+' SKS)') 
+            listCourse.append(course+" - "+course['Nama']) 
         st.subheader('Select Course(s)')
-        course_selected = st.multiselect('', options=course_list)
+        st.session_state['selectedCourse'] = st.multiselect('', options=listCourse)
+        
     with c2:
         for i in range(4):
             st.write(' ')
@@ -53,51 +52,30 @@ def FilterCourse():
         </style>""", unsafe_allow_html=True)
         
         if st.button('Create Plan'):
-            for course_name in course_selected:
-                for course in courses:
-                    if course['name'] == course_name[:-4]:
-                        course['selected'] = 'Yes'
-                        break
-            return True
+            sks = 0
+            for course in st.session_state['selectedCourse']:
+                sks += int(course[-14:-13])
+                print(sks)
+                if sks > 24:
+                    st.error('Melebihi Maksimum SKS ('+str(sks)+'/24)')
+                    return False
+                else st.success('Total SKS ('+str(sks)+'/24)')
+            st.session_state['menu'] = 'Choose Schedule'
+            return len(st.session_state['selectedCourse']) > 0
     
 def SelectSchedule():
     c1, c2 = st.columns ([3,1])
-    
+    db = firestore.client()
     with c1:
-        with st.expander("Course 1"):
-            course1_sched = ['Hari Pertama', 'Hari Kedua', 'Pilih sesuai rekomendasi dari JAKA']
-            cours1 = st.radio('Pilih Kelas yang Diinginkan:', course1_sched, key='cours1')
-    
-        with st.expander("Course 2"):
-            course2_sched = ['Hari Pertama', 'Hari Kedua', 'Pilih sesuai rekomendasi dari JAKA']
-            cours2 = st.radio('Pilih Kelas yang Diinginkan:', course2_sched, key='cours2')
-        
-        with st.expander("Course 3"):
-            course3_sched = ['Hari Pertama', 'Hari Kedua', 'Pilih sesuai rekomendasi dari JAKA']
-            cours3 = st.radio('Pilih Kelas yang Diinginkan:', course3_sched, key='cours3')
-    
-        with st.expander("Course 4"):
-            course4_sched = ['Hari Pertama', 'Hari Kedua', 'Pilih sesuai rekomendasi dari JAKA']
-            cours4 = st.radio('Pilih Kelas yang Diinginkan:', course4_sched, key='cours4')
-    
-        with st.expander("Course 5"):
-            course5_sched = ['Hari Pertama', 'Hari Kedua', 'Pilih sesuai rekomendasi dari JAKA']
-            cours5 = st.radio('Pilih Kelas yang Diinginkan:', course5_sched, key='cours5')
-    
-        with st.expander("Course 6"):
-            course6_sched = ['Hari Pertama', 'Hari Kedua', 'Pilih sesuai rekomendasi dari JAKA']
-            cours6 = st.radio('Pilih Kelas yang Diinginkan:', course6_sched, key='cours6')
-    
-        with st.expander("Course 7"):
-            course7_sched = ['Hari Pertama', 'Hari Kedua', 'Pilih sesuai rekomendasi dari JAKA']
-            cours7 = st.radio('Pilih Kelas yang Diinginkan:', course7_sched, key='cours7')
-    
-        with st.expander("Course 8"):
-            course8_sched = ['Hari Pertama', 'Hari Kedua', 'Pilih sesuai rekomendasi dari JAKA']
-            cours8 = st.radio('Pilih Kelas yang Diinginkan:', course8_sched, key='cours8')
-    
+        for course in st.session_state['selectedCourse']:
+            getData = db.collection('courses').document(course[:11]).get()
+            getData = getData.to_dict()
+            with st.expander(course):
+                courseDetails = getData['Kelas']
+                cours1 = st.radio('Pilih Kelas yang Diinginkan:', course1_sched, key='cours1')
+                
     with c2:
-        st.write("Jadwal yang telah dipilih per matkul")
+        st.subheader("Jadwal yang telah dipilih per matkul")
         st.write("")
         st.write("")
         st.write("Course 1:", cours1)
@@ -108,6 +86,3 @@ def SelectSchedule():
         st.write("Course 6:", cours6)
         st.write("Course 7:", cours7)
         st.write("Course 8:", cours8)
-
-def app():
-    return FilterCourse()
