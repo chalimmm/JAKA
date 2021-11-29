8cdce2b67a77bdcb94d0cfc3ff7ff9d785d8abbd
Chandra Halim Nuruddin <39824546+chalimmm@users.noreply.github.com>
2021-11-30 01:07:57 +0700

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -3,7 +3,7 @@ import pyrebase
 import hashlib
 import subprocess
 
-def authFirebase(u, p):
+def authFirebase(u, p): 
     # Configuration Key
     firebaseConfig = {
         'apiKey': "AIzaSyA-QKAvK7mW2P_Fvzmd__m2jrEXDb2Yg3M",
@@ -20,35 +20,40 @@ def authFirebase(u, p):
     # Firebase Authentication
     firebase = pyrebase.initialize_app(firebaseConfig)
     auth = firebase.auth()
-
-    # Database
-    db = firebase.database()
+    
+    with st.spinner('Authenticating...'):
+        subprocess.call(["python", "pages/scraping.py", u, p])
     
     if st.session_state['isAgree']:
         email = u + '@ui.ac.id'
-        passwd = hashlib.sha256(p.encode('utf-8')).hexdigest()
-        try:
-            user = auth.create_user_with_email_and_password(email, passwd)
-            data = {
-                email: {
-                    "username": u
-                }
-            }
-            
-            db.child("users").push(data, user['idToken'])
-        except:
-            pass
-        try:
-            user = auth.sign_in_with_email_and_password(email, passwd)
-            # st.success('Welcome ' + u)
-            st.session_state['logged_in'] = st.session_state['isAgree']
-            with st.spinner('Authenticating...'):
-                subprocess.call(["python", "pages/scraping.py", u, p])
-            st.session_state['menu'] = 'Settings'
-            st.session_state['temp'] = 1
-            st.success('Authenticated')
-        except:
-            st.warning('Authentication Failed')        
+        passwd = hashlib.sha256(u.encode('utf-8')).hexdigest()
+        st.session_state('username') = u
+        
+        ### FIRESTORE SECTION ###
+        import firebase_admin
+        from firebase_admin import credentials
+        from firebase_admin import firestore
+
+        cred = credentials.Certificate("serviceAccountKey.json")
+        firebase_admin.initialize_app(cred)
+
+        userData = db.collection('users').document(st.session_state['username']).get()
+        
+        if userData.exists:
+            try:
+                user = auth.create_user_with_email_and_password(email, passwd)
+            except:
+                pass
+            try:
+                user = auth.sign_in_with_email_and_password(email, passwd)
+                st.session_state['menu'] = 'Settings'
+                st.session_state['temp'] = 1
+                st.success('Authenticated')
+            except:
+                st.warning('Authentication Failed')
+        else:
+            st.warning('Authentication Failed')
+                
 def app():
     st.markdown("<h1 style='text-align: center;'>JAKA</h1>", unsafe_allow_html=True)
     st.markdown("<center><a style='text-align: center;'>Sign in to your SSO-UI account.</a></center>", unsafe_allow_html=True)
