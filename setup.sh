6de71acece23e8ea7b73d6929626e31607432cd1
Chandra Halim Nuruddin <39824546+chalimmm@users.noreply.github.com>
2021-11-29 20:00:32 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,11 @@ echo "[server]
 headless = true
 port = $PORT
 enableCORS = false
+
+[theme]
+base = 'light'
+primaryColor = '#f72585'
+backgroundColor = '#f8f8f8'
+textColor = '#3a0ca3'
+font = 'sans serif'
 " > ~/.streamlit/config.toml
